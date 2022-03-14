9be7db5c84a760d68689dcba6eafccff0b6267f1
lovelyorg <lovelyorg@126.com>
2022-03-14 12:13:35 +0800

    add leetcode

---
+++ .md/src/js/页面动态添加js文件.js
@@ -0,0 +1,5 @@
+https://res.wx.qq.com/open/js/jweixin-1.3.2.js
+
+var a = document.createElement('script')
+a.src='https://res.wx.qq.com/open/js/jweixin-1.6.0.js'
+document.getElementsByTagName('head')[0].appendChild(a)
